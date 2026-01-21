e6051ab99be0295377a4362634a58302dc926922
Santhi Swaroop Tirumalasetii <158589313+santhiswarooptirumalasetti@users.noreply.github.com>
2026-01-22 00:33:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,5 +30,8 @@ def convert_pdf():
     return send_file(docx_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
